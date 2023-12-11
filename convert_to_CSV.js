539ad39a544e4cfb9c54317656d3df43a77f7ad9
demczenko <134509169+demczenko@users.noreply.github.com>
2023-12-11 16:07:59 +0100

    Create convert_to_CSV.js

---
+++ convert_to_CSV.js
@@ -0,0 +1,30 @@
+const result = data.reduce((acc, item) => {
+    if (!pushed_countries.includes(item.country)) {
+        pushed_countries.push(item.country)
+        acc.push({
+            country: item.country,
+            [item.name]: item.name,
+            [item.name + "_src"]: item.src,
+            [item.name + "_url"]: item.href,
+            [item.name + "_main_id"]: item.main_id,
+            [item.name + "_highPrice"]: item.highPrice,
+            [item.name + "_lowPrice"]: item.lowPrice,
+        })
+    }
+
+    return acc.map(product => {
+        if (item.country === product.country) {
+            return {
+                ...product,
+                [item.name]: item.name,
+                [item.name + "_src"]: item.src,
+                [item.name + "_url"]: item.href,
+                [item.name + "_main_id"]: item.main_id,
+                [item.name + "_highPrice"]: item.highPrice,
+                [item.name + "_lowPrice"]: item.lowPrice,
+            }
+        }
+
+        return product
+    })
+}, [])
